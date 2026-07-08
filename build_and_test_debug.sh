c2e5ab2a80d2c2e36f3dc8f6d2f1a78dac4693e9
Hemmy <thehemmy123@gmail.com>
2026-07-08 10:19:40 +0100

    build_and_test_debug.sh script was not running debug versions of build

--- build_and_test_debug.sh
+++ build_and_test_debug.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 cmake -S . -B build-debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=g++-16
-cmake --build build
-ctest --test-dir build --output-on-failure
+cmake --build build-debug
+ctest --test-dir build-debug --output-on-failure
