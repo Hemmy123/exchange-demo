8b0d96754b3ca4dd0e6348cc807da71af6bc8278
Hemmy <thehemmy123@gmail.com>
2026-07-08 11:45:44 +0100

    Refactored MarketDataPublisher to use std::visit. I also only send out a MarketDataEvent for valid internal events (Trade/LevelChanged). Other internal events are kept but not broadcasted out to listeners for now

--- libs/marketdata/src/MarketDataPublisher.cpp
+++ libs/marketdata/src/MarketDataPublisher.cpp
@@ -4,24 +4,41 @@
 #include "MarketDataEvents.h"
 #include <variant>
 
+template <class... EventTypes> struct overload : EventTypes... {
+  using EventTypes::operator()...;
+};
+
 void MarketDataPublisher::Publish(const FeedMessage &ev) {
 
   MarketDataEvent marketDataEvent;
   marketDataEvent.seqNum = ev.seq;
   marketDataEvent.timeStamp = ev.timeStamp;
 
-  if (const auto *tradeEvent = std::get_if<TradeEvent>(&ev.payload)) {
-    marketDataEvent.instrumentId = tradeEvent->instrumentId;
-    marketDataEvent.body = MdTrade{.price = tradeEvent->restingPrice,
-                                   .qty = tradeEvent->quantityTraded,
-                                   .agressorSide = tradeEvent->aggressorSide,
-                                   .tradeId = tradeEvent->tradeId};
-  };
-  if (const auto *levelEvent = std::get_if<LevelChangedEvent>(&ev.payload)) {
-    marketDataEvent.instrumentId = levelEvent->instrumentId;
-    marketDataEvent.body = MdLevelUpdate{.side = levelEvent->side,
-                                         .price = levelEvent->price,
-                                         .totalQty = levelEvent->totalQty};
-  }
-  m_transport.Send(marketDataEvent);
+  std::visit(
+      overload{[&](TradeEvent event) {
+                 marketDataEvent.instrumentId = event.instrumentId;
+                 marketDataEvent.body =
+                     MdTrade{.price = event.restingPrice,
+                             .qty = event.quantityTraded,
+                             .agressorSide = event.aggressorSide,
+                             .tradeId = event.tradeId};
+                 m_transport.Send(marketDataEvent);
+               },
+
+               // We are only doing an L2 OrderBook for this project. So we
+               // don't need these 2 events. They exist internally but we do not
+               // need to send these out for external listeners. I'm keeping the
+               // internal events for reference and in case i want to update in
+               // future for learning purposes
+               [&](OrderAddedEvent event) {}, [&](OrderRemovedEvent event) {},
+
+               [&](LevelChangedEvent event) {
+                 marketDataEvent.instrumentId = event.instrumentId;
+                 marketDataEvent.body =
+                     MdLevelUpdate{.side = event.side,
+                                   .price = event.price,
+                                   .totalQty = event.totalQty};
+                 m_transport.Send(marketDataEvent);
+               }},
+      ev.payload);
 }
